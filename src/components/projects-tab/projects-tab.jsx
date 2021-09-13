a9a612c4e812319dd0c16921ca66ddcde8d843b3
Kevin Pierce <kevinpierce08@gmail.com>
2021-09-13 10:55:23 -0400

    Added golang project

--- src/components/projects-tab/projects-tab.jsx
+++ src/components/projects-tab/projects-tab.jsx
@@ -104,6 +104,38 @@ const ProjectsTab = () => {
                 <div className="flip-card">
                     <div className="flip-card-inner">
                         <div className="flip-card-front" >
+                        <Card id="golang-spotify">
+                            <Card.Body className="card-body">
+                                <Card.Text>
+                                    Go Spotify Playlist Builder
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </div>
+                        <div className="flip-card-back">
+                            <Card>
+                                <Card.Header>Go Spotify Playlist Builder</Card.Header>
+                                <Card.Body className="card-body">
+                                    <Card.Text>
+                                    A Golang CLI Application for building Spotify playlists based on Billboard Top 100 charts. Users can select which chart they'd like to scrape (rap, country, pop). 
+                                    <br/><br/>
+                                    Must be authorized on Spotify Developer console to use. 
+                                    </Card.Text>
+                                    <div class="icon-wrapper">
+                                        <a class="styled-icon" href="https://github.com/kevin-pierce/golang-playlist-builder" style={{justifySelf:"center"}}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front" >
                         <Card id="covid-bot">
                             <Card.Body className="card-body">
                                 <Card.Text>
